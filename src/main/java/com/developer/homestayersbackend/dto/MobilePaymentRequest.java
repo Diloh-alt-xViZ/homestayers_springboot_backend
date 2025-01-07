a8883cbaba6009396810e6d78a8ca90c15fdb7e8
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2025-01-07 17:35:07 +0200

    Add files via upload

---
+++ src/main/java/com/developer/homestayersbackend/dto/MobilePaymentRequest.java
@@ -0,0 +1,13 @@
+package com.developer.homestayersbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MobilePaymentRequest {
+    private String phoneNumber;
+    private String mobileProvider;
+}
