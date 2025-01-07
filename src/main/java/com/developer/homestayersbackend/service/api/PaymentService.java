9605bef46af493bf1497be1d6c63f7a0891ea345
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2025-01-07 17:31:07 +0200

    Update PaymentService.java

--- src/main/java/com/developer/homestayersbackend/service/api/PaymentService.java
+++ src/main/java/com/developer/homestayersbackend/service/api/PaymentService.java
@@ -1,4 +1,10 @@
 package com.developer.homestayersbackend.service.api;
 
-public interface PaymentService {
+import com.developer.homestayersbackend.dto.CardPaymentRequest;
+import com.developer.homestayersbackend.dto.MobilePaymentRequest;
+
+public interface PaymentsService {
+
+    String makeCardPayment(CardPaymentRequest request, Long userId);
+    String makePhonePayment(MobilePaymentRequest request, Long userId);
 }
